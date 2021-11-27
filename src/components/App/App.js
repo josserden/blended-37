109dfd8f89997b90ead9076a29a8e6420c944f0a
josserden <d-h11@ukr.net>
2021-11-27 15:58:39 +0200

    create data files

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,48 @@
-import './App.css';
+import "./App.styled.js";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { v4 as uuidv4 } from "uuid";
+
+import { Section, SectionTitle, Container, Button } from "./App.styled";
 
 function App() {
-  return <>?</>;
+  const handleClick = () => {
+    toast.success("🦄 Wow so easy!", {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  };
+
+  return (
+    <>
+      <Section>
+        <Container>
+          <SectionTitle>This is a Section title</SectionTitle>
+
+          <Button type="button" onClick={handleClick}>
+            This is a button
+          </Button>
+        </Container>
+      </Section>
+
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+    </>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "modern-normalize/modern-normalize.css";
+import { Global } from "@emotion/react";
+import { GlobalStyles } from "./index.styled";
+
+import App from "./components/App";
 
 ReactDOM.render(
   <React.StrictMode>
+    <Global styles={GlobalStyles} />
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );

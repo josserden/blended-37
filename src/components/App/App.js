615cdff3fc3933cd9c026fb8875eed068016cf0b
Den <52295780+josserden@users.noreply.github.com>
2021-12-19 11:11:41 +0200

    add components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,26 @@
-import "./App.styled.js";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import { v4 as uuidv4 } from "uuid";
+import './App.styled.js';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { v4 as uuidv4 } from 'uuid';
 
-import { Section, SectionTitle, Container, Button } from "./App.styled";
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
-function App() {
+import { Section, SectionTitle, Container } from './App.styled';
+
+function FormPropsTextFields() {
   const handleClick = () => {
-    toast.success("🦄 Wow so easy!", {
-      position: "top-right",
+    toast.success('🦄 Wow so easy!', {
+      position: 'top-right',
       autoClose: 5000,
       hideProgressBar: false,
       closeOnClick: true,
@@ -17,16 +29,107 @@ function App() {
       progress: undefined,
     });
   };
+  return (
+    <Box
+      component="form"
+      sx={{
+        '& .MuiTextField-root': { m: 1, width: '25ch' },
+      }}
+      noValidate
+      autoComplete="off">
+      <div>
+        <TextField
+          required
+          id="outlined-required"
+          label="Dessert"
+          defaultValue="Frozen yoghurt"
+        />
+        <TextField
+          required
+          id="outlined-required"
+          label="Calories"
+          defaultValue="159"
+        />
+        <TextField
+          required
+          id="outlined-required"
+          label="Fat"
+          defaultValue="6"
+        />
+        <TextField
+          required
+          id="outlined-required"
+          label="Carbs"
+          defaultValue="24"
+        />
+        <TextField
+          required
+          id="outlined-required"
+          label="Protein"
+          defaultValue="4"
+        />
+      </div>
 
+      <Button variant="contained" onClick={handleClick}>
+        Add
+      </Button>
+    </Box>
+  );
+}
+
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
+
+function BasicTable() {
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Dessert (100g serving)</TableCell>
+            <TableCell align="right">Calories</TableCell>
+            <TableCell align="right">Fat&nbsp;(g)</TableCell>
+            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.name}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">{row.calories}</TableCell>
+              <TableCell align="right">{row.fat}</TableCell>
+              <TableCell align="right">{row.carbs}</TableCell>
+              <TableCell align="right">{row.protein}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
+
+function App() {
   return (
     <>
       <Section>
         <Container>
-          <SectionTitle>This is a Section title</SectionTitle>
-
-          <Button type="button" onClick={handleClick}>
-            This is a button
-          </Button>
+          <SectionTitle>Phone Contacts</SectionTitle>
+          <BasicTable />
+          <FormPropsTextFields />
         </Container>
       </Section>
 
